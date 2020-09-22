55a2b17966b187fb6a92c8e81b5ff35675994371
Bernd OK <bowsnickiklewe@uwf.edu>
2020-09-21 19:26:00 -0500

    Generics working (-ish)

--- projects/Generics/src/generics/methods/GenericMthds1.java
+++ projects/Generics/src/generics/methods/GenericMthds1.java
@@ -1,6 +1,5 @@
 package generics.methods;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
--- projects/Generics/src/generics/methods/GenericMthds2.java
+++ projects/Generics/src/generics/methods/GenericMthds2.java
@@ -1,30 +1,31 @@
 package generics.methods;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collection;
-import java.util.List;
+
 
 public class GenericMthds2 {
-/*	
+
+	/*	KAPUT!
 	public static void fromArray(Object[] o, Collection<Object> c) {
 	   for (Object obj : o) {
 	      c.add(obj);
 	   }
 	   System.out.println();
 	}
-*/
-	public static <T> void fromArray(T[] o, Collection<T> c) {
-		   for (T obj : o) {
-		      c.add(obj);
-		   }
-		   System.out.println();
-		}
+	*/
 	
+	public static <T> void fromArray(T[] o, Collection<T> c) {
+		for (T obj : o) {
+			c.add(obj);
+		}
+		System.out.println();
+	}
+
 	public static void main(String[] args) {
 
-		String[] hello = {"hello", "World" };
+		String[] hello = { "hello", "World" };
 		Collection<String> c = new ArrayList<>();
-		fromArray(hello,c);
+		fromArray(hello, c);
 	}
 }

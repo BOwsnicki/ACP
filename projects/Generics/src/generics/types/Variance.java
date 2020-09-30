1cfa033e2d7daa5f658ec9af8b2fcbcadf1719b7
Bernd Owsnicki <bowsnickiklewe@uwf.edu>
2020-09-29 22:14:49 -0500

    Tiny but severe error in Generics project fixed

--- projects/Generics/src/generics/types/Variance.java
+++ projects/Generics/src/generics/types/Variance.java
@@ -1,17 +1,14 @@
 package generics.types;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import generics.model.A;
 import generics.model.B;
 
 public class Variance {
 	public static void main(String[] args) {
 		A[] arrayA = new B[100];	// ← Compiler? Runtime?
-		arrayA[0] = new B();		// ← Compiler? Runtime?
-
-		List<A> alA = new ArrayList<B>();
-		alA.add(new B());
+		arrayA[0] = new A();		// ← Compiler? Runtime?
+		
+//		List<A> alA = new ArrayList<B>();
+//		alA.add(new A());
 	}
 }

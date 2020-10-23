86f5c0865f9385ef7138ba370ad53cfea031bf54
Bernd Owsnicki <bowsnickiklewe@uwf.edu>
2020-10-23 12:58:58 -0500

    P4 GUI first steps

--- projects/project4SOLO/src/main/java/edu/uwf/acp/project4SOLO/App.java
+++ projects/project4SOLO/src/main/java/edu/uwf/acp/project4SOLO/App.java
@@ -22,6 +22,7 @@ import javafx.geometry.Insets;
 import javafx.geometry.VPos;
 import javafx.scene.control.*;
 import javafx.scene.control.Alert.AlertType;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.paint.Color;
@@ -30,34 +31,36 @@ import javafx.scene.text.Text;
 import javafx.scene.text.TextAlignment;
 
 public class App extends Application {
-
-	private static final int READY = 1;
-	private static final int RUNNING = 2;
-	private static final int TERMINATED = 3;
-
 	private static final String host = "127.0.0.1";
+	private static final int ME = 1;
+	private static final char EMPTY_CHAR = '.';
+	private static final char SERVER_CHAR = '0';
+	private static final char ME_CHAR = '1';
+	
 	private int port = 5000;
-	private BufferedReader stdIn;
 	private Socket socket;
 	private PrintWriter out;
 	private BufferedReader in;
-
+	private String mySymbol;
+	private String serverSymbol;
+	
 	private Canvas mainCanvas;
-	private int status;
 
+	public void sendRequest(String msg) {
+	  System.out.println("sending: " + msg);
+	  out.println(msg);
+	  out.flush();
+	}
+	
 	public static void main(String[] args) {
 		launch(args);
 	}
 	
 	private void drawSymbol (String sym, GraphicsContext gc, int x, int y) {
-		Text box = new Text(sym);
-		double width = box.getLayoutBounds().getWidth();
-		double height = box.getLayoutBounds().getHeight();
-		// gc.fillText(sym, x*100+100-(int)width, y*100+100-(int)height);
 		gc.fillText(sym, x*100+25, (y+1)*100-25);
 	}
 	
-	private void drawBoard(String board) {
+	private void drawBoard() {
 		GraphicsContext gc = mainCanvas.getGraphicsContext2D();
 		gc.setFont(new Font("Courier", 72));
 		gc.clearRect(0, 0, mainCanvas.getWidth(), mainCanvas.getHeight());
@@ -69,30 +72,34 @@ public class App extends Application {
 		gc.strokeLine(0,200,300,200);
 		gc.strokeLine(100,0,100,300);
 		gc.strokeLine(200,0,200,300);
-		drawSymbol("O",gc,0,2);
-		drawSymbol("X",gc,2,0);
-/*		
-		gc.strokeLine(xOff + ladderWidth, yOff, xOff + ladderWidth, yOff + 600);
-		for (int i = 1; i <= 20; i++) {
-			gc.strokeLine(xOff, yOff + 600 - 30 * i, xOff + ladderWidth, yOff + 600 - 30 * i);
-		}
-
-		gc.setTextAlign(TextAlignment.CENTER);
-		gc.setTextBaseline(VPos.CENTER);
-
-		gc.fillText(climber.getName(), Math.round(canvas.getWidth() / 2), 655);
-		*/
 	}
+
+	private void drawPlayers(String board) {
+		GraphicsContext gc = mainCanvas.getGraphicsContext2D();
+		gc.setFont(new Font("Courier", 72));
+		for (int i = 0; i < 9; i++) {
+			char player = board.charAt(i);
+			if (player == ME_CHAR) {
+				drawSymbol(mySymbol,gc,i%3,i/3);
+			} else {
+				if (player == SERVER_CHAR) {
+					drawSymbol(serverSymbol,gc,i%3,i/3);
+			}
+		}
+	}
+	}
+	
 	@Override
 	public void start(Stage primaryStage) {
 		primaryStage.setTitle("JavaFX TTT");
 		Group root = new Group();
 		GridPane outer = new GridPane();
 		GridPane grid = new GridPane();
-		int column = 1;
 		mainCanvas = new Canvas(300, 300);
+		EventHandler<MouseEvent> click = getCanvasClick();
+		mainCanvas.setOnMouseClicked(click);
+		drawBoard();
 		grid.add(mainCanvas, 1, 0);
-		status = READY;
 
 		Button startButton = new Button("Start");
 		EventHandler<ActionEvent> start = connectAndPlay();
@@ -111,10 +118,61 @@ public class App extends Application {
 		primaryStage.show();
 	}
 
-	private void play() {
-		drawBoard("");
+	private void doProlog() throws IOException {
+		String command = "play";
+		sendRequest(command);
+	  
+		String response = in.readLine();
+		System.out.println("server: " + response);
+	
+		// What's my symbol?
+		mySymbol = response.split(" ")[1];
+		serverSymbol = (mySymbol.equals("X") ? "O" : "X"); 
+		System.out.println("symbols: " + mySymbol + " " + serverSymbol);
 	}
 	
+	private void play() throws IOException {
+		// 1. Connect and see what's coming back
+		doProlog();
+        
+        // 2. Get going
+        boolean gameRunning = true;
+   		// 2a. Check if we really have game here
+   		sendRequest("status");
+   		String response = in.readLine();
+   		System.out.println("server: " + response);
+   		switch (response.split(" ")[0]) {
+   			case "draw":
+   			case "win": 	gameRunning = false;
+   							break;
+   			default:
+   		}
+   		requestBoard();
+      	}
+	}
+	
+	public EventHandler<MouseEvent> getCanvasClick() {
+		return new EventHandler<MouseEvent>() {
+			public void handle(MouseEvent e) {
+				int x = (int)Math.round(e.getX())/100;
+				int y = (int)Math.round(e.getY())/100;
+				System.out.println(x + " " + y);
+			}
+		};
+	}
+	
+      	private void showBoard(String board) {
+    		drawBoard();
+    		drawPlayers(board);      		
+      	}
+	
+      	private void requestBoard() throws IOException {
+            sendRequest("board");
+            String board = in.readLine();
+            System.out.println("server: " + board);
+            showBoard(board.split(" ")[1]);
+        }
+      	
 	public EventHandler<ActionEvent> connectAndPlay() {
 		return new EventHandler<ActionEvent>() {
 			public void handle(ActionEvent event) {
@@ -135,7 +193,12 @@ public class App extends Application {
 		              e.printStackTrace();
 		              System.exit(1);
 		          }
-		        	play();
+		        	try {
+						play();
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
 				}
 			};
 		}

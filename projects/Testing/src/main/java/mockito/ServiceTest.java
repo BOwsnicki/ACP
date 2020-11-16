dc3a623641531ec3b643c1c7fa5c459c53a425ad
Bernd Owsnicki <bowsnickiklewe@uwf.edu>
2020-11-15 20:03:20 -0600

    Advanced JUnit

---
+++ projects/Testing/src/main/java/mockito/ServiceTest.java
@@ -0,0 +1,27 @@
+package mockito;
+
+// https://www.baeldung.com/junit
+// https://github.com/eugenp/tutorials/tree/master/testing-modules/junit-5
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.platform.runner.JUnitPlatform;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.stubbing.Answer;
+
+@ExtendWith(MockitoExtension.class)
+@RunWith(JUnitPlatform.class)
+public class ServiceTest {
+//    UserService userService;
+}

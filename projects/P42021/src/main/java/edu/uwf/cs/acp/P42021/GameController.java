21fed7058a612f7de59f62c5f17526fee40c53a9
BerndOK <bowsnickiklewe@uwf.edu>
2021-10-27 20:30:33 -0500

    P4 new more work

--- projects/P42021/src/main/java/edu/uwf/cs/acp/P42021/GameController.java
+++ projects/P42021/src/main/java/edu/uwf/cs/acp/P42021/GameController.java
@@ -6,27 +6,112 @@ import java.util.List;
 import java.util.Scanner;
 
 public class GameController {
-	private List<String> words;
+	private static List<String> words;
+	private String chosenWord;
 
-	public GameController() {
+	static {
 		words = new ArrayList<>();
 		try {
 			Scanner s = new Scanner(new File("words.txt"));
 			while (s.hasNextLine()) {
-				String st = s.nextLine();
-				insert(st);
+				words.add(s.nextLine().toUpperCase());
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-
-	private void insert(String s) {
-		words.add(s);
-	}
 	
+	public GameController() {
+		reset();
+	}
+
+	public void reset() {
+		chosenWord = randomString();
+	}
 	private String randomString() {
 		int index = (int)(Math.random()*words.size());
 		return words.get(index);
+	}
+	
+	public String clue() {
+		return chosenWord.charAt(0) + "____";
+	}
+	
+	public String respondTo(String guess) {
+		if (guess.length() != 5) {
+			return "_____";
+		}
+		String response = "";
+		for (int i = 0; i < 5; i++) {
+			char c = guess.charAt(i);
+			if (chosenWord.charAt(i) == c) {
+				response += c;
+			} else {
+				if (chosenWord.indexOf(c) >= 0) {
+					response += ("" + c).toLowerCase();
+				} else {
+					response += "_";
+				}
+			}
+		}
+		return response;
+	}
+	
+	public int correct(String state) {
+		int count = 0;
+		for (int i = 0; i < state.length(); i++) {
+			if (state.charAt(i) >= 'A' && state.charAt(i) <= 'Z') count++;
+		}
+		return count;
+	}
+	private String replaceChar(String str, char ch, int index) {
+	    return str.substring(0, index) + ch + str.substring(index+1);
+	}
+	public String merge(String s1, String s2) {
+		String merged = "_____";		
+		for (int i = 0; i < s1.length(); i++) {
+			if (s1.charAt(i) >= 'A' && s1.charAt(i) <= 'Z') {
+				merged = replaceChar(merged,s1.charAt(i),i);
+			}
+			if (s2.charAt(i) >= 'A' && s2.charAt(i) <= 'Z') {
+				merged = replaceChar(merged,s2.charAt(i),i);
+			}
+		}
+		return merged;
+	}
+	
+	public String display(String state) {
+		String top = "|";
+		for (int i = 0; i < state.length(); i++) {
+			top += (" " + state.charAt(i)).toUpperCase() + " |";
+		}
+		String middle = "+---+---+---+---+---+";
+		String bottom = "|";
+		for (int i = 0; i < state.length(); i++) {
+			if (state.charAt(i) >= 'a' && state.charAt(i) <= 'z') {
+				bottom += " ? |";
+			} else {
+				bottom += "   |";
+			}
+		}
+		return middle + "\n" + top + "\n" + middle + "\n" + bottom + "\n" + middle;
+	}
+	
+	public static void main(String[] args) {
+		GameController gc = new GameController();
+		Scanner in = new Scanner(System.in);
+		String state = gc.clue();
+		String best = state;
+		System.out.println(state);
+		System.out.println(best);
+		while (gc.correct(state) < 5) {
+			System.out.print("> ");
+			String guess = in.nextLine();
+			state = gc.respondTo(guess);
+			System.out.println(gc.display(state));
+			best = gc.merge(best,state);
+			System.out.println(state);
+			System.out.println(best);
+		}	
 	}
 }
